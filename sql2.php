c63cda7bf4693c6b067181f114b668116d9c7370
Ruben <rubenpaolini@mailfence.com>
2024-10-16 14:50:41 -0400

    fdsfsd

--- sql2.php
+++ sql2.php
@@ -12,6 +12,31 @@
 
 <p>Brand: <?= htmlspecialchars($_POST['brand']) ?></p>
 
+
+<?php
+        $brand = htmlspecialchars($_POST['brand'])
+        $server = "localhost";
+        $username = "php";
+        $password = "hello";
+        $database = "bubemedia";
+        $conn = mysqli_connect($server, $username, $password, $database);
+
+        // Check for successful connection
+        if (!$conn) {
+            die("Connection failed: " . mysqli_connect_error());
+        }
+
+        $sql = "select hp from courses where brand='{$brand}';";
+        $result = mysqli_query($conn, $sql);
+    ?>
+
+    <p>
+        You selected brand: <?= $brand ?>.<br/>
+        <?php
+        mysqli_close($conn);
+        ?>
+    </p>
+
 </body>
 
 </html>
