64b3e86465fbee80ec832b563cf10d7b51229845
Ruben <rubenpaolini@mailfence.com>
2024-10-16 14:52:37 -0400

    sad

--- sql2.php
+++ sql2.php
@@ -4,7 +4,23 @@
 
 <head>
 
-<title></title>
+<title>SQLtest</title>
+<?php
+        $brand = htmlspecialchars($_POST['brand'])
+        $server = "localhost";
+        $username = "php";
+        $password = "hello";
+        $database = "bubemedia";
+        $conn = mysqli_connect($server, $username, $password, $database);
+
+        // Check for successful connection
+        if (!$conn) {
+            die("Connection failed: " . mysqli_connect_error());
+        }
+
+        $sql = "select hp from courses where brand='{$brand}';";
+        $result = mysqli_query($conn, $sql);
+        ?>
 
 </head>
 
