b889a3c0839481066be12248f8e9925c064d5964
Ruben <rubenpaolini@mailfence.com>
2024-10-16 14:41:05 -0400

    sfsdfsd

---
+++ sql.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>SQL test</title>
+        <?php
+        $brand = htmlspecialchars($_POST['brand'])
+        $server = "localhost";
+        $username = "php";
+        $password = "hello";
+        $database = "bubemedia";
+        $conn = mysqli_connect($server, $username, $password, $database);
+
+        // Check for successful connection
+        if (!$conn) {
+            die("Connection failed: " . mysqli_connect_error());
+        }
+
+        $sql = "select hp from courses where brand='{$brand}';";
+        $result = mysqli_query($conn, $sql);
+        ?>
+    </head>
+    <body>
+        You  <?= $brand ?>.<br/>
+        <?php
+        foreach ($result as $row) // There should only be one row returned
+        {
+            echo "{$row['brand']} has {$row['hp']} horsepower.";
+        }
+        // Don't forget to close the connection!
+        mysqli_close($conn);
+        ?>
+    </body>
+</html>
