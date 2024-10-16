2b9f1fbb2fcce6f900e8ed4e191269fdbf689eef
Ruben <rubenpaolini@mailfence.com>
2024-10-16 15:13:31 -0400

    asds

--- sql.php
+++ sql.php
@@ -1,34 +1,26 @@
 <!DOCTYPE html>
 <html>
-    <head>
+
+<head>
         <title>SQL test</title>
         <?php
-        $brand = htmlspecialchars($_POST['brand'])
-        $server = "localhost";
-        $username = "php";
-        $password = "hello";
-        $database = "bubemedia";
-        $conn = mysqli_connect($server, $username, $password, $database);
-
-        // Check for successful connection
-        if (!$conn) {
-            die("Connection failed: " . mysqli_connect_error());
-        }
-
-        $sql = "select hp from courses where brand='{$brand}';";
-        $result = mysqli_query($conn, $sql);
+            $brand = htmlspecialchars($_GET["brand"]);
+            $server = "localhost";
+            $username = "php";
+            $password = "hello";
+            $database = "bubemedia";
+            $conn = mysqli_connect($server, $username, $password, $database);
+            
+            // Check for successful connection
+            if (!$conn) {
+              die("Connection failed: {mysqli_connect_error()}");
+            }
+            $sql = "select * from cars;";
+            $result = mysqli_query($conn, $sql);
         ?>
-    </head>
+</head>
+
     <body>
-        You selected brand: <?= $brand ?>.<br/>
-        <?php
-      //  foreach ($result as $row) // There should only be one row returned
-      //  {
-     //       echo "{$row['brand']} has {$row['hp']} horsepower.";
-       // }
-        // Don't forget to close the connection!
-        foreach($result as $row) { echo "id: {$row["id"]} | Car: {$row["brand"]} {$row["make"]}\n"; }
-        mysqli_close($conn);
-        ?>
+    You selected brand <?= $brand ?>.<br/>
     </body>
 </html>
