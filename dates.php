57552fee6ce202400a9a6aead8bf34269967dc23
bubeTUBE10 <rubenpaolini@mailfence.ocm>
2024-10-17 21:33:50 -0400

    dsd

--- dates.php
+++ dates.php
@@ -22,18 +22,37 @@
 
 </p>
 <form action="sql.php" method="get">
-  <label for="year">year:</label><br />
-  <select id="year" name="year">
-                <?php
-                    foreach($result as $row) 
-                    {
-                        echo "<option value='{$row['year']}'>{$row['year']}</option>\n";
-                    }
-                    // Don't forget to close the connection!
-                    mysqli_close($conn);
-                ?>
-            </select>
+  <label for="year">Year:</label><br />
+  <select id="year" name="year" onchange="fetchMonths()">
+    <option value="">Select Year</option>
+    <?php
+      foreach($result as $row) {
+        echo "<option value='{$row['year']}'>{$row['year']}</option>\n";
+      }
+    ?>
+  </select><br />
+
+  <label for="month">Month:</label><br />
+  <select id="month" name="month">
+    <option value="">Select Month</option>
+    <!-- Months will be populated dynamically based on the selected year -->
+  </select><br />
+
   <input type="submit" value="Submit">
 </form>
+
+<script>
+  function fetchMonths() {
+    var year = document.getElementById("year").value;
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "get_months.php?year=" + year, true);
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState == 4 && xhr.status == 200) {
+        document.getElementById("month").innerHTML = xhr.responseText;
+      }
+    };
+    xhr.send();
+  }
+</script>
 <p>
 </html>
